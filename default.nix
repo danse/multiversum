ea6249aa0457e6ab80ad9538a1300dcb613c2f32
danse <f.occhipinti@gmail.com>
2016-09-01 18:13:42 +0200

    remove the Nix conf file, i am now using a global one

--- default.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> {}; {
-  sdlEnv = stdenv.mkDerivation {
-    name = "blog";
-    # for cabal to find the header file (zlib.h)
-    buildInputs = [ stdenv zlib ];
-    # for cabal to find the shared object (zlib.so)
-    LD_LIBRARY_PATH = "${zlib}/lib";
-  };
-}
